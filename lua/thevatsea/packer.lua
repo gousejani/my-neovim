31d7f059c639bff99c8d96ffe56ca906243ffbd5
gouse13 <gousejani13@gmail.com>
2024-05-25 23:07:45 +0530

    added new packages, key maps

--- lua/thevatsea/packer.lua
+++ lua/thevatsea/packer.lua
@@ -4,86 +4,107 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use 'prettier/vim-prettier'
-  
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use 'prettier/vim-prettier'
 
-  use {
-      'nvim-tree/nvim-tree.lua',
-      requires = {
-          'nvim-tree/nvim-web-devicons', -- optional
-      },
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use 'voldikss/vim-floaterm'
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+    use 'voldikss/vim-floaterm'
 
-  use {
-      'morhetz/gruvbox', 
-      as = 'gruvbox',
-      config = function() 
-          vim.cmd.colorscheme("gruvbox")
-      end
-  }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-  use {
-      'folke/tokyonight.nvim', 
-      as = 'tokyonight',
-      config = function() 
-          vim.cmd.colorscheme("tokyonight-night")
-      end
-  }
+    use {
+        'rebelot/kanagawa.nvim',
+        as = 'kanagawa',
+        config = function() 
+            vim.cmd.colorscheme("kanagawa")
+        end
+    }
 
-  use({ 'stevedylandev/flexoki-nvim', as = 'flexoki' })
+    use {
+        'morhetz/gruvbox', 
+        as = 'gruvbox',
+        config = function() 
+            vim.cmd.colorscheme("gruvbox")
+        end
+    }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('ThePrimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
+    use {
+        'folke/tokyonight.nvim', 
+        as = 'tokyonight',
+        config = function() 
+            vim.cmd.colorscheme("tokyonight-night")
+        end
+    }
+
+    use({ 'stevedylandev/flexoki-nvim', as = 'flexoki' })
+
+    use { 
+        'catppuccin/nvim', 
+        as = 'catppuccin',
+        config = function()
+            vim.cmd.colorscheme('catppuccin')
+        end
+    }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment these if you want to manage LSP servers from neovim
-		  -- {'williamboman/mason.nvim'},
-		  -- {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            -- {'williamboman/mason.nvim'},
+            -- {'williamboman/mason-lspconfig.nvim'},
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  }
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 
-  use {
-	  "williamboman/mason.nvim",
-	  "williamboman/mason-lspconfig.nvim",
-	  "neovim/nvim-lspconfig",
-  }
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
 
-  use {
-      'numToStr/Comment.nvim',
-      config = function()
-          require('Comment').setup()
-      end
-  }
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
 
-  end)
+end)
--- lua/thevatsea/remap.lua
+++ lua/thevatsea/remap.lua
@@ -64,3 +64,40 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+
+-- Floaterm
+
+vim.keymap.set("n", "<leader>to", function()
+    vim.cmd("FloatermNew")
+end)
+
+vim.keymap.set("n", "<leader>tt", function()
+    vim.cmd("FloatermToggle!")
+end)
+
+vim.keymap.set("t", "<leader>tt", function()
+    vim.cmd("FloatermToggle!")
+end)
+
+vim.keymap.set("n", "<leader>tc", function()
+    vim.cmd("FloatermKill!")
+end)
+
+vim.keymap.set("t", "<leader>tc", function()
+    vim.cmd("FloatermKill!")
+end)
+
+
+-- Disable arrow keys in insert mode
+vim.api.nvim_set_keymap('i', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Right>', '<Nop>', { noremap = true, silent = true })
+
+-- Disable arrow keys in normal mode
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true })
+
